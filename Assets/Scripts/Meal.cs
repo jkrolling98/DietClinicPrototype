8f9ce7a4c9e7cfb40f59c12895ecb3ab976e50ad
jkrolling98 <65852623+jkrolling98@users.noreply.github.com>
2023-01-11 22:52:17 +0800

    added meal scriptable object

---
+++ Assets/Scripts/Meal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Meal", menuName = "ScriptableObjects/Meal", order = 1)]
+public class Meal : ScriptableObject
+{
+    public string dishName;
+    public Sprite image;
+    [TextArea]
+    public string description;
+    public float cost;
+    public int wholeGrainServings;
+    public int proteinServings;
+    public int veggieServings;
+    public float calories; //in kcal
+    public float protein; // in g
+    public float totalFats; //in g
+    public float saturatedFats; // in g
+    public float dietaryFibre; // in g
+    public float carbohydrate; // in g
+    public float cholesterol; //in mg
+    public float sodium; //in mg
+}
